4c84ffd8b605c51244c4ec204aad0232f3085e0c
frdrk16 <146846928+frdrk16@users.noreply.github.com>
2024-06-10 12:29:28 +0100

    initial commit of v1_main.py file

---
+++ main_v1.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np
+from configparser import ConfigParser
+
+class BiddingSim():
+    def __init__(self, config: ConfigParser):
+        print('Main class')
+
+
+if __name__ == '__main__':
+    x = Main()
